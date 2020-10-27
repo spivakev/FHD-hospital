75232c077751b1a8bbc74bcc9d7406038f0a3633
Elena Spivak <spivak.e.v@gmail.com>
2020-10-27 21:06:58 +0300

    feat: add Sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './fonts/fonts.css';
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Sidebar />
     </div>
   );
 }
