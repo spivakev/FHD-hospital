9eabb141519750d09d4210e99c4235ff2b2465c8
Elena Spivak <spivak.e.v@gmail.com>
2020-10-27 14:21:27 +0300

    feat: header icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import HeaderIcons from '../HeaderIcons'
+import Avatar from '../Avatar'
 import './style.css'
+import avatar from '../../img/avatar.png'
 
 
 export default class Header extends Component {
@@ -15,16 +17,23 @@ export default class Header extends Component {
           </div>
           <div className="header__right">
 
-            <form>
+            <form className="header__form">
               <input className="header__search" type="text" placeholder="Поиск по медкарте" />
             </form>
 
-            <div className="header__todo"></div>
-            <div className="header__chat"></div>
-            <div className="header__notification"></div>
-            <div className="header__avatar"></div>
+            <div className="header__todo header__icon">
+              <HeaderIcons iconKey="todo" />
+            </div>
+            <div className="header__chat header__icon">
+              <HeaderIcons iconKey="chat" />
+            </div>
+            <div className="header__notification header__icon">
+              <HeaderIcons iconKey="notification" />
+            </div>
+            <div className="header__avatar">
+              <Avatar src={avatar} size="52" />
+            </div>
           </div>
-
 
         </div>
       </div>
@@ -32,5 +41,3 @@ export default class Header extends Component {
   }
 
 };
-
-//export default Header;
